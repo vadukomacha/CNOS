620884f649dadc1d65ee09ebaf054f0f97c4673f
vadukomacha <vadukomacha9@gmail.com>
2026-03-25 23:20:12 +0530

    last and final

--- Week10indexed.c
+++ Week10indexed.c
@@ -1,48 +1,57 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-int main() 
+#include <stdio.h>
+#include <stdlib.h>
+int main()
 {
-int n, size, i; 
-int free_blocks[100];
-for(i=0;i<100;i++) free_blocks[i]=0; 
+    int n, size, i;
+    int free_blocks[100];
+    for (i = 0; i < 100; i++)
+        free_blocks[i] = 0;
 
-printf("Enter number of files: ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-printf("Enter size of file %d: ", i+1);
-scanf("%d",&size);
-int index_block=-1;
-int j;
+    printf("Enter number of files: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter size of file %d: ", i + 1);
+        scanf("%d", &size);
+        int index_block = -1;
+        int j;
 
-for(j=0;j<100;j++)
-{
-if(free_blocks[j]==0) { index_block=j; free_blocks[j]=1; break; }
-}
-if(index_block==-1) { 
-printf("No free block for index\n"); 
-continue; 
-}
-int *blocks = (int*)malloc(size*sizeof(int)); 
-int count=0;
-for(j=0;j<100 && count<size;j++)
-{
-if(free_blocks[j]==0) { 
-blocks[count]=j; 
-free_blocks[j]=1; 
-count++; 
-}
-}
-if(count<size) { 
-printf("Not enough free blocks\n"); 
-free(blocks); 
-continue;
-}
-printf("File %d allocated. Index block: %d, Data blocks: ", i+1, index_block);
-for(j=0;j<size;j++) 
-printf("%d ", blocks[j]);
-printf("\n");
-free(blocks); 
-}
-return 0;
+        for (j = 0; j < 100; j++)
+        {
+            if (free_blocks[j] == 0)
+            {
+                index_block = j;
+                free_blocks[j] = 1;
+                break;
+            }
+        }
+        if (index_block == -1)
+        {
+            printf("No free block for index\n");
+            continue;
+        }
+        int *blocks = (int *)malloc(size * sizeof(int));
+        int count = 0;
+        for (j = 0; j < 100 && count < size; j++)
+        {
+            if (free_blocks[j] == 0)
+            {
+                blocks[count] = j;
+                free_blocks[j] = 1;
+                count++;
+            }
+        }
+        if (count < size)
+        {
+            printf("Not enough free blocks\n");
+            free(blocks);
+            continue;
+        }
+        printf("File %d allocated. Index block: %d, Data blocks: ", i + 1, index_block);
+        for (j = 0; j < size; j++)
+            printf("%d ", blocks[j]);
+        printf("\n");
+        free(blocks);
+    }
+    return 0;
 }
--- Week10linked.c
+++ Week10linked.c
@@ -1,47 +1,51 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-struct Node 
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
 {
-int block; 
-struct Node *next; 
+    int block;
+    struct Node *next;
 };
-int main() 
+int main()
 {
-int n, size, i; 
-int free_blocks[100]; 
-for(i=0;i<100;i++) free_blocks[i]=0; 
-printf("Enter number of files: ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-printf("Enter size of file %d: ", i+1);
-scanf("%d",&size);
-struct Node *head=NULL, *temp, *newnode;
-int j, count=0;
-for(j=0;j<100 && count<size;j++)
-{
-if(free_blocks[j]==0) // if block is free
-{
+    int n, size, i;
+    int free_blocks[100];
+    for (i = 0; i < 100; i++)
+        free_blocks[i] = 0;
+    printf("Enter number of files: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter size of file %d: ", i + 1);
+        scanf("%d", &size);
+        struct Node *head = NULL, *temp, *newnode;
+        int j, count = 0;
+        for (j = 0; j < 100 && count < size; j++)
+        {
+            if (free_blocks[j] == 0) // if block is free
+            {
 
-newnode=(struct Node*)malloc(sizeof(struct Node)); 
-newnode->block=j; 
-newnode->next=NULL; 
-if(head==NULL) head=temp=newnode; 
-else { 
-temp->next=newnode; temp=newnode; 
-}
-free_blocks[j]=1; 
-count++;
-}
-}
-printf("File %d allocated blocks: ", i+1);
-temp=head;
-while(temp!=NULL)
-{
-printf("%d ", temp->block); 
-temp=temp->next; 
-}
-printf("\n");
-}
-return 0;
+                newnode = (struct Node *)malloc(sizeof(struct Node));
+                newnode->block = j;
+                newnode->next = NULL;
+                if (head == NULL)
+                    head = temp = newnode;
+                else
+                {
+                    temp->next = newnode;
+                    temp = newnode;
+                }
+                free_blocks[j] = 1;
+                count++;
+            }
+        }
+        printf("File %d allocated blocks: ", i + 1);
+        temp = head;
+        while (temp != NULL)
+        {
+            printf("%d ", temp->block);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+    return 0;
 }
